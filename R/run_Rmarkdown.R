a87d190daf47e9271390208df1a5100c4b1a4983
MartinBulla <bulla.mar@gmail.com>
2021-02-24 20:09:20 +0100

    update

--- R/run_Rmarkdown.R
+++ R/run_Rmarkdown.R
@@ -5,4 +5,4 @@ rmarkdown::render('R/Preregistration.Rmd', output_dir = 'Protocols', word_docume
 #rmarkdown::render('R/Preregistration.Rmd', output_dir = 'Protocols', word_document(highlight = "zenburn"))
 
 rmarkdown::render('R/protocol_sperm.Rmd', output_dir = 'Protocols')
-rmarkdown::render('R/protocol_sperm.Rmd', output_dir = 'Protocols', word_document())
+rmarkdown::render('R/protocol_sperm.Rmd', output_dir = 'Protocols', word_document(toc = TRUE))
