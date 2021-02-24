3df5abe6f7cb4753de3b3ff7b28ede0c6e55a376
MartinBulla <bulla.mar@gmail.com>
2021-02-24 18:35:55 +0100

    Update run_Rmarkdown.R

--- R/run_Rmarkdown.R
+++ R/run_Rmarkdown.R
@@ -1,7 +1,7 @@
 require(rmarkdown)
 
 rmarkdown::render('R/Preregistration.Rmd', output_dir = 'Protocols')
-rmarkdown::render('R/Preregistration.Rmd', output_dir = 'Protocols', word_document())
+rmarkdown::render('R/Preregistration.Rmd', output_dir = 'Protocols', word_document(toc = TRUE))
 #rmarkdown::render('R/Preregistration.Rmd', output_dir = 'Protocols', word_document(highlight = "zenburn"))
 
 rmarkdown::render('R/protocol_sperm.Rmd', output_dir = 'Protocols')
