6bda6f6beba345c8bb9a4d412eb4a0c1f7ffa0f4
MartinBulla <bulla.mar@gmail.com>
2021-08-11 20:43:11 +0200

    Update Notes.R

--- R/Notes.R
+++ R/Notes.R
@@ -1,3 +1,11 @@
+OUR REPEATABILITY sample:
+- Batch of 40 sperm (20 males, 2 sperm per mail)
+- worth having few "funny" ones in there
+
+To do
+select test images and share those with me, I will rename them so that noone can track, which sperm belongs to what mail, but will keep the metadate to link them during analyses- I will then share with you a copy of these images and you can make the dots (preferably similar size as used by SpermSizeer) for the start and ends of each part in ImageJ (having them in one layer - so that those can be turned on and off). This we then use to compare the measurements in ImageJ (one of you connecting the dots) with those made by SpermSizer (either of you clicking on the dots).
+- Clearly, before this is done, agreeing on the way to measure the sperm (i.e. about decisions where each part starts/ends) and adding that info into the protocol is essential. If helpful, we can certainly discuss that before I leave. Then (likely after I am back) the one doing the dots shall measure in ImageJ and either of you can then measure in SpermSizer, as it requires just clicking the dots :-) (and perhaps adjusting the images). Then we need to decide whether we shall do repeatability of the ImageJ measurements or (if SpermSizer corresponds well with ImageJ) doing it only in SpermSizer will do.
+
 15 sobota vecer, 16 samplovat, nebo
 16 nedele prijezd, 17 sample, 18 odjezd
 7-8 June
@@ -5,6 +13,30 @@
 TOM Repeatability
 - 2 sperm from 75 individuals
 - repeatability 93-98% 
+
+Info Tom
+
+a) 10 sperm/bird
+b) take best sperm
+c) careful, if the tail is whole (fades away and is not covered by bubble - i.e. we know the end)
+
+
+Repeatability Hanka - email from Mon, 4 Sept 2017, 19:28 and from Tue, 5 Sept 2017, 11:22 and from 
+Tue, 5 Sept 2017, 11:41
+
+
+pk - je id spermie a jak vidis vysvetluje 95-98%. Ja myslim, ze je to ok, ne?
+
+     model     type   effect estimate_r
+1     HEAD % of var       pk         96
+2     HEAD % of var Residual          4
+3 midpiece % of var       pk         95
+4 midpiece % of var Residual          5
+5     tail % of var       pk         98
+6     tail % of var Residual          2
+
+
+KIM 
 
 ·         I measured 13 samples from 15.  I couldn’t measure 2 samples, 1670 and 1306.  There were few sperm in the samples and they were very thin, mostly broken and didn’t take up the stain very well.  In particular the heads wouldn’t stain which indicates no DNA.  I don’t know why.  I tried a few times with these samples and always got the same result.
 ·         Sample 1650 was very small and unlike the other samples – it didn’t look like a VD tubule.  I did manage to get a few sperm though, but I had to scan two entire slides to find about 20 good sperm (from a good sample I get that many from a small patch).  The sperm looked good though and no different from the other samples.
