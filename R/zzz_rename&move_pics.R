508d2e850a2def3fb9aac30f9b867140b06048e1
MartinBulla <bulla.mar@gmail.com>
2021-10-04 16:07:12 +0200

    Update zzz_rename&move_pics.R

--- R/zzz_rename&move_pics.R
+++ R/zzz_rename&move_pics.R
@@ -10,16 +10,22 @@
   d = d[!grepl('metadata', f2, fixed = TRUE)]
   d[ , sample_ID := substring(f2,6,8)]
   d[, file_name :=sub(".*/", "", f2)]
-  d[nchar(file_name)<32]
-  d[sample_ID %in% c('400','401'), file_name := paste(substring(file_name,1,8), '2021-04-19', substring(file_name,10), sep =" ")]
-  d[sample_ID %in% c('402'), file_name := paste(substring(file_name,1,8), '2021-04-20', substring(file_name,10), sep =" ")]
+  #d[nchar(file_name)<32]
+  d[sample_ID %in% c('400'), file_name := paste(substring(file_name,1,8), '2021-04-19', substring(file_name,10))]
+  d[sample_ID %in% c('401'), file_name := paste(substring(file_name,1,9), '2021-04-19', substring(file_name,11))]
+  d[sample_ID %in% c('402'), file_name := paste(substring(file_name,1,8), '2021-04-20', substring(file_name,11))]
   
+  #paste(substring('Ruff 320 Snap-591.jpg',1,8), '2021-04-20', substring('Ruff 320 Snap-591.jpg',10))
+
+  #dd = d[sample_ID %in% c('400')]
   d[, new_name := paste0("Ruff ", sample_ID, "_", substring(file_name, 6))]
-  d[substring(new_name, nchar(new_name)-7, nchar(new_name)-7) == "-", file_name := gsub("-", "-0", file_name)]
+  d[substring(new_name, nchar(new_name)-8, nchar(new_name)-7) == "p-", new_name := gsub("p-", "p-0", new_name)]
+  
+  #d$new_name
   
   # rename & copy
   for(i in 1:nrow(d)){
-    #i = 1 
+    i = 1 
     file.copy(from = d$f[i], to = glue('all_photos/',d$new_name[i])) 
   }
 
@@ -34,6 +40,8 @@
   #d[nchar(file_name)<32]
   
   d[, new_name := paste0("Ruff ", sample_ID, "_", substring(file_name, 6))]
+  #d[substring(new_name, nchar(new_name)-8, nchar(new_name)-7) == "p-", new_name := gsub("p-", "p-0", new_name)]
+  
   
   # rename & copy
   for(i in 1:nrow(d)){
