099a3c045a3fc6832328631c0aff74cd912ba811
MartinBulla <bulla.mar@gmail.com>
2022-05-16 16:14:00 +0200

    added clustering

--- R/EXP_morpho.R
+++ R/EXP_morpho.R
@@ -414,6 +414,10 @@ knitr::opts_chunk$set(message = FALSE, warning = FALSE, cache = TRUE)
       ha[, Midpiece_rel := ar[part == 'Midpiece',.(Length_rel)]]
       ha[, Flagellum_rel := ar[part == 'Flagellum',.(Length_rel)]]
 
+    # export for Mihai
+      a$CV = cv_$CV[match(a$bird_ID, cv_$bird_ID)]
+      #save(b,br,a,ar, file = 'Data/ruff_sperm_for_Mihai.RData')
+
 #' ## Basic info
 #' - for each male 10 sperm measured from one of the two sampling occasions
 #' - motility for most individuals and for many in May and June
@@ -3712,4 +3716,74 @@ knitr::opts_chunk$set(message = FALSE, warning = FALSE, cache = TRUE)
           width = 18, height =8, units = 'cm') 
 
 #' <span style="color: red;">!!! Perhaps a tendency for longer acrosomes making slower sperm and longer other traits making faster sperm !!!</span>  
+
+#' Clustering
+  require('apcluster') 
+  require('Rcpp')                                                                                                                       
+  bw[, ID := .I]
+  aw[, ID := .I]
+
+  get_clusters <- function(apc) {
+    x <- lapply(apc@clusters, function(x) data.table(ID = names(x) %>% as.numeric()))
+    for (i in 1:length(x)) x[[i]][, clust_ID := i]
+    rbindlist(x)
+  }
+
+  #  all data
+    apc = apclusterK
+    apc <- apcluster(corSimMat(r = 1), bw[, .(VAP, VSL, VCL, Acrosome, Flagellum, Head, Midpiece, Nucleus, Tail, Total, Midpiece_rel, Flagellum_rel)], q = 0 ,  details = TRUE)
+    apc = apclusterK(corSimMat(r = 1), bw[, .(VAP, VSL, VCL, Acrosome, Flagellum, Head, Midpiece, Nucleus, Tail, Total, Midpiece_rel, Flagellum_rel)],  K = 3,  details = TRUE)
+    
+    apc <- apcluster(corSimMat(r = 1), bw[, .(Head, Midpiece, Tail, Total)], q = 0 ,  details = TRUE)
+    apc = apclusterK(corSimMat(r = 1), bw[, .(Acrosome, Flagellum, Head, Midpiece, Nucleus, Tail, Total)],  K = 3,  details = TRUE)
+
+
+    apc <- apcluster(negDistMat(r=2) , bw[, .(VAP, VSL, VCL, Acrosome, Flagellum, Head, Midpiece, Nucleus, Tail, Total, Midpiece_rel, Flagellum_rel)], q = 0 ,  details = TRUE)
+    apc = apclusterK(negDistMat(r = 2), bw[, .(VAP, VSL, VCL, Acrosome, Flagellum, Head, Midpiece, Nucleus, Tail, Total, Midpiece_rel, Flagellum_rel)],  K = 3,  details = TRUE)
+    apc <- apcluster(expSimMat(r=2) , bw[, .(VAP, VSL, VCL, Acrosome, Flagellum, Head, Midpiece, Nucleus, Tail, Total, Midpiece_rel, Flagellum_rel)], q = 0 ,  details = TRUE)
+    #apc = apclusterK(expSimMat(r = 2), bw[, .(VAP, VSL, VCL, Acrosome, Flagellum, Head, Midpiece, Nucleus, Tail, Total, Midpiece_rel, Flagellum_rel)],  K = 3,  details = TRUE)
+    apc <- apcluster(linKernel() , bw[, .(VAP, VSL, VCL, Acrosome, Flagellum, Head, Midpiece, Nucleus, Tail, Total, Midpiece_rel, Flagellum_rel)], q = 0 ,  details = TRUE)
+
+    cc <- get_clusters(apc)
+
+    O <- merge(cc, bw, by = "ID")
+
+    #O[, .N, .(clust_ID, Morph)]
+
+    #dev.new()
+    ggplot(O, aes(x = clust_ID, y = Morph)) +
+    geom_count()
+
+    plot(apc, bw[, .(VAP, VSL, VCL, Acrosome, Flagellum, Head, Midpiece, Nucleus, Tail, Total, Midpiece_rel, Flagellum_rel)])
+    heatmap(apc)
+
+#  averages   
+    apc <- apcluster(corSimMat(r = 1), aw[, .(VAP, VSL, VCL, Acrosome, Flagellum, Head, Midpiece, Nucleus, Tail, Total, Midpiece_rel, Flagellum_rel)], q = 0 ,  details = TRUE)
+     apc = apclusterK(corSimMat(r = 1), aw[, .(VAP, VSL, VCL, Acrosome, Flagellum, Head, Midpiece, Nucleus, Tail, Total, Midpiece_rel, Flagellum_rel)],  K = 3,  details = TRUE)
+    
+    apc <- apcluster(corSimMat(r = 1), aw[, .(Head, Midpiece, Tail, Total)], q = 0 ,  details = TRUE)
+    apc <- apclusterK(corSimMat(r = 1), aw[, .(Head, Midpiece, Tail, Total)], K = 3 ,  details = TRUE)
+
+    apc <- apcluster(corSimMat(r = 1), aw[, .(VAP, VSL, VCL)], q = 0 ,  details = TRUE)
+    apc <- apclusterK(corSimMat(r = 1), aw[, .(VAP, VSL, VCL)], K = 3 ,  details = TRUE)
+
+    apc <- apcluster(negDistMat(r=2) , aw[, .(VAP, VSL, VCL, Acrosome, Flagellum, Head, Midpiece, Nucleus, Tail, Total, Midpiece_rel, Flagellum_rel)], q = 0.5 ,  details = TRUE)
+     apc = apclusterK(negDistMat(r = 2), aw[, .(VAP, VSL, VCL, Acrosome, Flagellum, Head, Midpiece, Nucleus, Tail, Total, Midpiece_rel, Flagellum_rel)],  K = 3,  details = TRUE)
+    
+    apc <- apcluster(expSimMat(r=2) , aw[, .(VAP, VSL, VCL, Acrosome, Flagellum, Head, Midpiece, Nucleus, Tail, Total, Midpiece_rel, Flagellum_rel)], q = 0 ,  details = TRUE)
+     
+     apc = apclusterK(expSimMat(r = 2), aw[, .(VAP, VSL, VCL, Acrosome, Flagellum, Head, Midpiece, Nucleus, Tail, Total, Midpiece_rel, Flagellum_rel)],  K = 3,  details = TRUE)
+
+    cc <- get_clusters(apc)
+
+    O <- merge(cc, aw, by = "ID")
+
+    O[, .N, .(clust_ID, Morph)]
+
+    #dev.new()
+    ggplot(O, aes(x = clust_ID, y = Morph)) +
+    geom_count()
+
+    plot(apc, aw[, .(Head, Midpiece, Tail, Total)])
+    heatmap(apc)
 # End     
